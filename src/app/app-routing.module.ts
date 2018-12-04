5fe453a6b006716665335957fd25b528b4b5a29a
Matheus Henrique <mathsoares1@gmail.com>
2018-12-04 09:57:17 -0300

    Alterado nome da pasta favorites

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'top-news', loadChildren: './top-news/top-news.module#TopNewsPageModule' },
+  { path: 'headlines', loadChildren: './headlines/headlines.module#HeadlinesPageModule' },
+  { path: 'sources', loadChildren: './sources/sources.module#SourcesPageModule' },
+  { path: 'favourites', loadChildren: './favourites/favourites.module#FavouritesPageModule' },
+  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
+  { path: 'favorites', loadChildren: './favorites/favorites.module#FavoritesPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
